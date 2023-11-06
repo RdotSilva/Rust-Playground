379f1f6a4f8551a2aa0992d5a5891aec92bfdb63
RdotSilva <RyandotSilva@gmail.com>
2023-11-06 19:46:34 +0000

    Add tests

--- src/my_functs.rs
+++ src/my_functs.rs
@@ -6,3 +6,18 @@ pub fn add_five(num: u32) -> u32 {
 pub fn sum_array(arr: &[i32]) -> i32 {
     arr.iter().sum()
 }
+
+#[cfg(test)]
+mod test {
+    // Inherit all modules that are imported
+    use super::*;
+
+    #[test]
+    fn add_five_test() {
+        let x: u32 = 100;
+        let y: u32 = add_five(x);
+
+        println!("x and y are from test: {} {}", x, y);
+        assert_eq!(y, 105);
+    }
+}
--- src/other_functs/minus_functs.rs
+++ src/other_functs/minus_functs.rs
@@ -1,3 +1,18 @@
 pub fn subtract_10(num: u32) -> u32 {
     num - 10
 }
+
+#[cfg(test)]
+mod test {
+    // Inherit all modules that are imported
+    use super::*;
+
+    #[test]
+    fn subtract_10_test() {
+        let x: u32 = 100;
+        let y: u32 = subtract_10(x);
+
+        println!("x and y are from test: {} {}", x, y);
+        assert_eq!(y, 90);
+    }
+}
