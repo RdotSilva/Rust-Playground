1f526fa462ab9e0a72a417c77302ae3afe477dce
RdotSilva <RyandotSilva@gmail.com>
2023-11-11 12:47:13 +0000

    Add starter examples

--- basic_playground/src/main.rs
+++ basic_playground/src/main.rs
@@ -1,3 +1,28 @@
+const OUR_COURSE: &str = "Rust Playground";
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello World {}", OUR_COURSE);
+
+    // Stack
+    let x: i32;
+    x = 2;
+    println!("x is {}", x);
+
+    let y: i32 = 4;
+    println!("y is {}", y);
+
+    // For loop
+    for i in 0..=y {
+        if i != 4 {
+            print!("{}, ", i)
+        } else {
+            print!("{}, ", i)
+        }
+    }
+
+    // Mutation
+    let mut z: i32 = 5;
+    print!("z was {} ", z);
+    z = 10;
+    println!("but is now {}", z);
 }
