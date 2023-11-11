036c27347d8d31ed3378ef221c7687eb24959623
RdotSilva <RyandotSilva@gmail.com>
2023-11-11 12:53:00 +0000

    Add more basic examples

--- basic_playground/src/main.rs
+++ basic_playground/src/main.rs
@@ -25,4 +25,27 @@ fn main() {
     print!("z was {} ", z);
     z = 10;
     println!("but is now {}", z);
+
+    // Float
+    let freezing_temp: f64 = -2.4;
+    println!("freezing_temp is {}", freezing_temp);
+
+    // Modulo
+    let is_zero_remainder: bool = 10 % 4 != 0;
+    println!("is_zero_remainder is {}", is_zero_remainder);
+
+    // Char (single quotes for chars)
+    let my_char: char = 'z';
+    println!("my_char is {}", my_char);
+
+    let emoji_char: char = '😎';
+    println!("emoji_char is {}", emoji_char);
+
+    // Float array stored on the sack (fixed size)
+    let my_floats: [f32; 10] = [0.0; 10];
+    println!("my_floats is {:?}", my_floats);
+
+    // Map over an array
+    let my_floats_new: [f32; 10] = my_floats.map(|n| n + 2.0);
+    println!("my_floats_new is {:?}", my_floats_new);
 }
