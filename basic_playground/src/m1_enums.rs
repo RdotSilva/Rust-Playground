7f8bfdd16264f595fa7d31874586f591afa4bd27
RdotSilva <RyandotSilva@gmail.com>
2023-11-15 18:05:53 +0000

    Add some or none generic examples

--- basic_playground/src/m1_enums.rs
+++ basic_playground/src/m1_enums.rs
@@ -13,6 +13,12 @@ enum GivenResult<T, E> {
     Err(E),
 }
 
+#[derive(Debug)]
+enum GivenOption<T> {
+    None,
+    Some(T),
+}
+
 fn create_car_colour_blue() -> CarColour {
     let my_car_colour: CarColour = CarColour::Blue;
     my_car_colour
@@ -24,6 +30,16 @@ fn check_under_five(num_check: u8) -> GivenResult<u8, String> {
         GivenResult::Ok((num_check))
     } else {
         GivenResult::Err(("Not under 5!".to_string()))
+    }
+}
+
+// Some or none generic example
+fn remainder_zero(num_check: f32) -> GivenOption<f32> {
+    let remainder: f32 = num_check % 10.0;
+    if remainder != 0.0 {
+        GivenOption::Some(remainder)
+    } else {
+        GivenOption::None
     }
 }
 
@@ -41,5 +57,8 @@ mod test {
 
         let under_five_res = check_under_five(7);
         dbg!(under_five_res);
+
+        let remainder: GivenOption<f32> = remainder_zero(12.2);
+        dbg!(remainder);
     }
 }
