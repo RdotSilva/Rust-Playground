a3132e2a0f7c3ed76456bd6e918c943b0bb635d8
RdotSilva <RyandotSilva@gmail.com>
2023-11-15 17:39:32 +0000

    Add CarColour enum example

--- basic_playground/src/m1_enums.rs
+++ basic_playground/src/m1_enums.rs
@@ -1,9 +1,24 @@
+#[derive(Debug)]
+#[allow(dead_code)]
+enum CarColour {
+    Red,
+    Green,
+    Blue,
+    Silver,
+}
+
+fn create_car_colour_blue() -> CarColour {
+    let my_car_colour: CarColour = CarColour::Blue;
+    my_car_colour
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn tests_enums() {
-        dbg!("Hello Enums");
+        let car_colour: CarColour = create_car_colour_blue();
+        dbg!(car_colour);
     }
 }
