178067c712d34c8c0035aae6b99bc8740941142f
RdotSilva <RyandotSilva@gmail.com>
2023-11-16 19:49:06 +0000

    Add struct creation example

--- basic_playground/src/m2_structs.rs
+++ basic_playground/src/m2_structs.rs
@@ -1,9 +1,25 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn tests_structs() {
-        dbg!("Hello Structs");
+        // Create a new user
+        let user_1: User = User {
+            username: String::from("Someone"),
+            email: String::from("someone@example.com"),
+            active: true,
+            sign_in_count: 1,
+        };
+
+        dbg!(user_1);
     }
 }
