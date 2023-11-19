752f3002a34e0db92fe7ce84cad189a1de66ff49
RdotSilva <RyandotSilva@gmail.com>
2023-11-19 16:14:50 +0000

    Add trait and string implementation

--- basic_playground/src/m4_polymorphism.rs
+++ basic_playground/src/m4_polymorphism.rs
@@ -1,7 +1,23 @@
 #[cfg(test)]
 mod test {
- 
-  
-  #[test]
- 
+    use ethers::types::Address;
+    use std::str::FromStr;
+
+    trait EthereumAddress {
+        fn convert_address(&self) -> Result<Address, &'static str>;
+    }
+
+    // Call the implementation of Ethereum Address for a string reference type
+    impl EthereumAddress for &str {
+        fn convert_address(&self) -> Result<Address, &'static str> {
+            match Address::from_str(self) {
+                Ok(address) => Ok(address),
+                Err(_) => Err("Invalid Ethereum Address String"),
+            }
+        }
+    }
+
+   
+    #[test]
+   
 }
