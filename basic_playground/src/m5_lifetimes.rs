ae9cc6257bc2770f13e22edab645e14927796f9d
RdotSilva <RyandotSilva@gmail.com>
2023-11-21 17:15:34 +0000

    Add dereferencing example_1

--- basic_playground/src/m5_lifetimes.rs
+++ basic_playground/src/m5_lifetimes.rs
@@ -7,3 +7,28 @@ fn example_0() {
 
     println!("r: {}", r);
 }
+
+#[allow(dead_code, unused_variables)]
+fn example_1() {
+    // Allocate space in memory
+    let highest_age: i32;
+
+    // Initialize vars
+    let alice_age: i32 = 20; // 'a
+    let bob_age: i32 = 21; // 'b: 'a
+
+    // Call function
+    highest_age = largest(&alice_age, &bob_age);
+
+    // Print output
+    println!("Highest age is {}", highest_age);
+
+    // This returns an i32 so we have to de-reference compare_1 and compare_2 when returning
+    fn largest(compare_1: &i32, compare_2: &i32) -> i32 {
+        if compare_1 > compare_2 {
+            *compare_1
+        } else {
+            *compare_2
+        }
+    }
+}
