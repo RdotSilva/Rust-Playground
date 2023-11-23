618222cde847c8fff0dd1993616c485a059307e6
RdotSilva <RyandotSilva@gmail.com>
2023-11-23 17:38:02 +0000

    Add option match example

--- basic_playground/src/m6_patterns.rs
+++ basic_playground/src/m6_patterns.rs
@@ -13,4 +13,27 @@ mod test {
 
         println!("{}", res);
     }
+
+    #[test]
+    fn tests_match_option() {
+        let some_num: Option<i32> = Some(10);
+
+        let my_int: i32 = if let Some(i) = some_num {
+            i
+        } else {
+            panic!("There was a problem");
+        };
+
+        println!("My int: {}", my_int);
+
+        // let res = match some_num {
+        //   Some(i) => i,
+        //   None => {
+        //     panic!("There was a problem");
+        //   }
+        // };
+
+        // println!("{:?}", some_num);
+        // println!("{}", res);
+    }
 }
