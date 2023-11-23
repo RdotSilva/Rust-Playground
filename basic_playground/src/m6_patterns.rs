8a153ba54f531b5ad727c3889d22da7870295dd4
RdotSilva <RyandotSilva@gmail.com>
2023-11-23 17:34:19 +0000

    Add literals match example

--- basic_playground/src/m6_patterns.rs
+++ basic_playground/src/m6_patterns.rs
@@ -2,5 +2,15 @@
 mod test {
 
     #[test]
+    fn tests_match_literals() {
+        let number: i32 = 20;
 
+        let res: &str = match number {
+            1 => "It was the first!",
+            2 | 3 | 5 | 7 | 15 | 20 => "We found it in the sequence!",
+            _ => "It was something else!",
+        };
+
+        println!("{}", res);
+    }
 }
