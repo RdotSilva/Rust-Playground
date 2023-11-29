49be10d90dc4cfa6ba27c956b5cc00e652a7a603
RdotSilva <RyandotSilva@gmail.com>
2023-11-29 19:16:26 +0000

    Add declarative macro test

--- basic_playground/src/m9_decl_macros.rs
+++ basic_playground/src/m9_decl_macros.rs
@@ -1,2 +1,13 @@
 #[cfg(test)]
-mod tests {}
+mod tests {
+
+    #[test]
+    fn tests_declarative_macro() {
+        let mut _x: Vec<i32> = vec![];
+        dbg!(_x);
+
+        let formatted: String = format!("Hello with vec {:?}", _x);
+
+        dbg!(formatted);
+    }
+}
