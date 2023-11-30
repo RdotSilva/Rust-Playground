faf07537c65c6314eb9294db3b1fa3a388583797
RdotSilva <RyandotSilva@gmail.com>
2023-11-30 20:31:13 +0000

    Add macro capture comments

--- basic_playground/src/m9_decl_macros.rs
+++ basic_playground/src/m9_decl_macros.rs
@@ -1,3 +1,36 @@
+// MACRO CAPTURES
+
+/* expr
+    matches to a valid rust expression
+    "hello".to_string(), vec![1, 2, 3], 1 + 2, 1
+*/
+
+/* stmt
+    matches to a rust statement
+    let x = 5, x.push(1), return Some(x)
+*/
+
+/* ident
+    matches to a rust identifier
+    variable name, function name, module name
+*/
+
+/* ty
+    matches to a rust type
+    i32, Vec<String>, Option<T>
+*/
+
+/* path
+    matches to a rust path
+    std::collections::HashMap
+*/
+
+// Repetition SPECIFIER
+
+// * - match zero or more repetitions
+// + - match one or more repetitions
+// ? - Match zero or one repetitions
+
 #[cfg(test)]
 mod tests {
 
