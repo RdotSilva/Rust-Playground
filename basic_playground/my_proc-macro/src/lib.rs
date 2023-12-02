733d32a73877022b1908afa07a2c686f38df468e
RdotSilva <RyandotSilva@gmail.com>
2023-12-02 13:59:36 +0000

    Fix build error

--- basic_playground/my_proc-macro/src/lib.rs
+++ basic_playground/my_proc-macro/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate proc_macro;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, ItemFn};
