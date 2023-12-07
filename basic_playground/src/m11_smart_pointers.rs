55def951bb7305da2faa5f8f9c699894b8db320e
RdotSilva <RyandotSilva@gmail.com>
2023-12-07 18:28:31 +0000

    Add box smart pointer example

--- basic_playground/src/m11_smart_pointers.rs
+++ basic_playground/src/m11_smart_pointers.rs
@@ -2,8 +2,26 @@
 mod tests {
 
     #[test]
-    fn tests_box_smart_pointer() {}
+    #[allow(dead_code, unused_variables)]
+    fn tests_box_smart_pointer() {
+        #[derive(Debug)]
+        struct Node {
+            id: u32,
+            next: Option<Box<Node>>,
+        }
+
+        let nodes: Box<Node> = Box::new(Node {
+            id: 0,
+            next: Some(Box::new(Node {
+                id: 1,
+                next: Some(Box::new(Node { id: 2, next: None })),
+            })),
+        });
+
+        dbg!(nodes);
+    }
 
     #[test]
+    #[allow(dead_code, unused_variables)]
     fn tests_reference_counter() {}
 }
